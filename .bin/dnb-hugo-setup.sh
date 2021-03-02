fc975f37ba202f0ecd9335df804364e501c1107d
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-03-03 00:00:38 +0700

    chore: update setup scripts

--- .bin/dnb-hugo-setup.sh
+++ .bin/dnb-hugo-setup.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/env bash
+
+arrayName=(
+  "browserslist-config"
+  "commitlint-config"
+  "eslint-config"
+  "remark-config"
+  "standard-version-config"
+  "stylelint-config"
+  "textlint-config"
+)
+
+for i in "${arrayName[@]}"; do
+  [ ! -d "$1" ] && git clone git@github.com:dnb-hugo/"${1}".git
+  cd "$i" || return
+  git pull
+  git update
+  if [ -f package.json ]; then
+    npm install
+  fi
+  cd ..
+done
